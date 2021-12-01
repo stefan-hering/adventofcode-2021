e72fb9518f017d79f94ee702c67febd3e699c912
Stefan Hering <stefan.hering@rewe-digital.com>
2021-12-01 09:18:23 +0100

    Day01

---
+++ day01/day01.sql
@@ -0,0 +1,14 @@
+-- part 1
+select count(diff) from (select depths - lag(depths, 1) over () as diff from day01) d where diff > 0;
+
+-- part 2
+select count(diff)
+from (
+         select (depths +
+                 lag(depths, 1) over() +
+                 lag(depths, 2) over()) -
+                (lag(depths, 1) over() +
+                 lag(depths, 2) over() +
+                 lag(depths, 3) over()) as diff
+         from day01) d
+where diff > 0;
