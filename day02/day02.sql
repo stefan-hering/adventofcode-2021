4fa0fa357ce54d88cac8ffb2e5b4b60a471827a2
Stefan Hering <stefan.hering@rewe-digital.com>
2021-12-02 08:41:55 +0100

    Day02 refactoring

--- day02/day02.sql
+++ day02/day02.sql
@@ -1,32 +1,23 @@
 select *
 from day02;
 
+create or replace view movements as
+select case
+           when direction = 'forward' then value
+           end              as forward,
+       case
+           when direction = 'up' then value * - 1
+           when direction = 'down' then value
+           end              as height,
+       row_number() over () as rownum
+from day02;
+
 -- part 1
-with movements as (
-    select case
-               when direction = 'forward' then value
-               end as forward,
-           case
-               when direction = 'up' then value * - 1
-               when direction = 'down' then value
-               end as height
-    from day02)
 select sum(height) * sum(forward)
 from movements;
 
-
 -- part 2
 with angles as (
-    with movements as (
-        select case
-                   when direction = 'forward' then value
-                   end                        as forward,
-               case
-                   when direction = 'up' then value * - 1
-                   when direction = 'down' then value
-                   end                        as height,
-               row_number() over (order by 1) as rownum
-        from day02)
     select forward,
            coalesce(sum(height) over (order by rownum), 0) as angle
     from movements)
