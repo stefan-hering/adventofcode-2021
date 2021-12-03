ecaf348afc0b5a7381d51152d9fb2fc6e173afc3
Stefan Hering <stefan.hering@rewe-digital.com>
2021-12-03 14:46:40 +0100

    Day03 Failed part 2 attempt 2

--- day03/day03-attempt2.sql
+++ day03/day03-attempt2.sql
@@ -38,3 +38,25 @@ select gammaRate.rate::bit(12)::integer * epsilonRate.rate::bit(12)::integer
 from gammaRate,
      epsilonRate;
 
+
+-- part 2, this approach might work, but can't use the recursive cte in a subquery :(
+with recursive indexedDay3 as (select number, row_number() over () as id from day03),
+               oxygen as (
+                   select *, 1 as depth
+                   from indexedDay3
+                   union all
+                   select indexedDay3.*, depth + 1
+                   from indexedDay3
+                            join oxygen on indexedDay3.id = oxygen.id
+                   where substr(oxygen.number, depth, 1) = (select bit
+                                                            from (select substr(number, depth, 1)::char as bit
+                                                                  from oxygen o2
+                                                                  where o2.depth = oxygen.depth) bits
+                                                            group by 1
+                                                            order by count(*) desc, bit desc fetch first row only)
+                     and depth <= (select length(number) from day03 fetch first row only)
+               )
+select number
+from oxygen
+order by depth desc fetch first row only;
+
