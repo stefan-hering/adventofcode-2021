728c00bdce4f6c6e8cd0c6c22654d2e032397beb
Stefan Hering <stefan.hering@rewe-digital.com>
2021-12-06 10:21:23 +0100

    Day05 Cleanup

--- day05/day05.sql
+++ day05/day05.sql
@@ -1,27 +1,19 @@
 -- part 1
-with seriesy as (
-    select *,
-           generate_series(y1 * 1000 + x1, y2 * 1000 + x1, 1000) l,
-           generate_series(y2 * 1000 + x1, y1 * 1000 + x1, 1000) r
-    from day05
-    where x1 = x2),
-     seriesx as (select generate_series(x1 + y1 * 1000, x2 + y1 * 1000) l,
-                        generate_series(x2 + y1 * 1000, x1 + y1 * 1000) r
-                 from day05
-                 where y1 = y2),
-     pipeCoordinates as (
-         select case when l is null then r else l end coordinate
-         from seriesy
-         union all
-         select case when l is null then r else l end coordinate
-         from seriesx),
+with directions as (
+    select *,case when x1 < x2 then 1 when x1 > x2 then -1 else 0 end xdir,
+           case when y1 < y2 then 1000 when y1 > y2 then -1000 else 0 end ydir
+    from day05 where x1 = x2 or y1 = y2),
+     coordinates as (
+         select generate_series(x1 + y1 * 1000, x2 + y2 * 1000, xdir+ydir) coordinate from directions
+     ),
      summedCoordinates as (
          select coordinate, count(coordinate) count
-         from pipeCoordinates
+         from coordinates
          group by coordinate)
 select count(0)
 from summedCoordinates
 where count > 1;
+
 
 -- part 2
 with directions as (
